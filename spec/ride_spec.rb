5510f3ee80716bd056d9d69080562294377f8934
Samuel Cox <samc1253@gmail.com>
2022-11-14 09:34:02 -0700

    Add method to return total revenue

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Visitor do
   let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
   let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
   let(:visitor3) { Visitor.new('Penny', 64, '$15') }
+  before :each do
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+  end
 
   it 'exists and has attributes' do
     expect(ride1).to be_a Ride
@@ -13,5 +17,9 @@ RSpec.describe Visitor do
     expect(ride1.min_height).to eq 24
     expect(ride1.admission_fee).to eq 1
     expect(ride1.excitement).to eq (:gentle)
+  end
+
+  it 'can return the ride total revenue as 0 before any rides' do
+    expect(ride.total_revenue).to eq 0
   end
 end
