57bd7075b49d2d5c5ed9b2ec2a9991687e236f26
Samuel Cox <samc1253@gmail.com>
2022-11-14 09:34:10 -0700

    Add method to return total revenue

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,9 +1,14 @@
 class Ride
-  attr_reader :name, :min_height, :admission_fee, :excitement
+  attr_reader :name, :min_height, :admission_fee, :excitement, :rider_log
   def initialize(data)
     @name = data[:name]
     @min_height = data[:min_height]
     @admission_fee = data[:admission_fee]
     @excitement = data[:excitement]
+    @rider_log = {}
+  end
+
+  def total_revenue
+    0
   end
 end
