4bdb3d35cd553489db74b2cf07ab1cebedbbd6a7
Samuel Cox <samc1253@gmail.com>
2022-11-14 10:07:38 -0700

    Add carnival spec file and lay test framework

---
+++ spec/carnival_spec.rb
@@ -0,0 +1,23 @@
+require './lib/visitor'
+require './lib/ride'
+
+RSpec.describe Visitor do
+  let(:carnival) { Carnival.new('05-27-2023')}
+  let(:ride1) { Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle }) }
+  let(:ride2) { Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle }) }
+  let(:ride3) { Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling }) }
+  let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
+  let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
+  let(:visitor3) { Visitor.new('Penny', 64, '$15') }
+  before :each do
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:thrilling)
+  end
+
+  it 'exists and has no rides by default' do
+    expect(carnival).to be_a Carnival
+    expect(carnival.rides).to eq([])
+  end
+end
