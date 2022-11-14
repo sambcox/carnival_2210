7eb0606ecc255eaeed09d3d52a0b62da8320518a
Samuel Cox <samc1253@gmail.com>
2022-11-14 09:30:11 -0700

    Add ride class and allow to be initialized

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,9 @@
 class Ride
-
+  attr_reader :name, :min_height, :admission_fee, :excitement
+  def initialize(data)
+    @name = data[:name]
+    @min_height = data[:min_height]
+    @admission_fee = data[:admission_fee]
+    @excitement = data[:excitement]
+  end
 end
