f2aa1904290f0e976c066ec1e13f9bfd7ab20cb8
Samuel Cox <samc1253@gmail.com>
2022-11-14 09:14:59 -0700

    Add preferences

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,9 +1,10 @@
 class Visitor
-  attr_reader :name, :height
+  attr_reader :name, :height, :preferences
   def initialize(name, height, spending_money)
     @name = name
     @height = height
     @spending_money = spending_money
+    @preferences = []
   end
 
   def spending_money
