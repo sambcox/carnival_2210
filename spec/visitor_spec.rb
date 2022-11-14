9378d5610c65688738c15a006d1d8e7003cd724c
Samuel Cox <samc1253@gmail.com>
2022-11-14 09:09:20 -0700

    Create visitor spec file and lay testing framework

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,12 @@
+require './lib/visitor'
+
+RSpec.describe Visitor do
+  let(:visitor) { Visitor.new('Bruce', 54, '$10') }
+
+  it 'exists and has attributes' do
+    expect(visitor).to be_a Visitor
+    expect(visitor.name).to eq('Bruce')
+    expect(visitor.height).to eq 54
+    expect(visitor.spending_money).to eq('$10')
+  end
+end
